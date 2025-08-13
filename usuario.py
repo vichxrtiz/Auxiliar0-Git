1d8923306141623da1c819ac3bf6814ec34a4b91
vicente ortiz leyton <vichortizleyton@gmail.com>
2025-08-13 19:11:49 -0400

    refactor: change in method

--- usuario.py
+++ usuario.py
@@ -11,6 +11,5 @@ class Usuario:
     def listarTareas(self):
         for tarea in self.tareas:
             if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
+                print(f"La tarea {tarea.obtenerNombre()} está lista")
+                print(f"La tarea {tarea.obtenerNombre()} no está lista")
